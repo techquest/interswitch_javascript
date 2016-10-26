4a15e5c7595132fbdc569e7506e04dc2bdcae883
Okeowo Aderemi <AOkeowo@Interswitchng.com>
2016-10-26 20:06:02 +0100

    * Added Send Stub,SendwithAccessToken Stub and setEnviroment

--- src/Interswitch.js
+++ src/Interswitch.js
@@ -1,2 +1,32 @@
+/**
+ * Interswitch SDK
+ */
 class Interswitch {
+    constructor(clientid, secret) {
+        this.clientid = clientid;
+        this.clientSecret = secret;
+        this.InterswitchUrl = {
+            PASSPORT_OAUTH_RESOURCE_URL: "passport/oauth/token",
+            PRODUCTION_BASE_URL: "https://saturn.interswitchng.com/"
+        };
+        this.InterswitchEnv = {
+            ENV_SANDBOX: "SANDBOX",
+            ENV_PRODUCTION: "PRODUCTION"
+        };
+    }
+    /**
+     * Sets the Environment
+     */
+    setEnvironment(environmentMode) {
+        if (environmentMode === this.InterswitchEnv.ENV_SANDBOX) {
+            this.environment = this.InterswitchEnv.ENV_SANDBOX;
+        }
+        else if (environmentMode === this.InterswitchEnv.ENV_PRODUCTION) {
+            this.environment = this.InterswitchEnv.ENV_PRODUCTION;
+        }
+    }
+    sendWithAccessToken(url, method, datam = httpHeaders, signedParameters) {
+    }
+    send(url, method, datam = httpHeaders, signedParameters) {
+    }
 }
--- src/Interswitch.ts
+++ src/Interswitch.ts
@@ -1,4 +1,59 @@
+/**
+ * Interswitch SDK 
+ */
+
+interface InterswitchEnvironment {
+    ENV_SANDBOX: String;
+    ENV_PRODUCTION: String;
+
+}
+
+
 class Interswitch {
+    clientid: String;
+    clientSecret: String;
+    environment: String;
+    passportBaseUrl: String;
+    serviceUrl: Object;
+    InterswitchEnv: InterswitchEnvironment;
+    InterswitchUrl: Object;
+
+    constructor(clientid: String, secret: String) {
+        this.clientid = clientid;
+        this.clientSecret = secret;
+        this.InterswitchUrl = {
+            PASSPORT_OAUTH_RESOURCE_URL: "passport/oauth/token",
+            PRODUCTION_BASE_URL: "https://saturn.interswitchng.com/"
+        }
+
+        this.InterswitchEnv = {
+            ENV_SANDBOX: "SANDBOX",
+            ENV_PRODUCTION: "PRODUCTION"
+        }
+
+    }
+    /**
+     * Sets the Environment 
+     */
+    setEnvironment(environmentMode: String) {
+        if (environmentMode === this.InterswitchEnv.ENV_SANDBOX) {
+            this.environment = this.InterswitchEnv.ENV_SANDBOX
+        }
+        else if (environmentMode === this.InterswitchEnv.ENV_PRODUCTION) {
+            this.environment = this.InterswitchEnv.ENV_PRODUCTION
+        }
+    }
+
+
+sendWithAccessToken(url,method,datam httpHeaders, signedParameters){
+
+}   
+send(url,method,datam httpHeaders, signedParameters){
+    
+}   
+    
+
+
 
 
 }
